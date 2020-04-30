a14dfaa9f7bc2603f09d31fef8f900f128332374
doppert <60747570+doppert@users.noreply.github.com>
2020-05-01 00:06:27 +0700

    Add files via upload

---
+++ install_notion.php
@@ -0,0 +1,5 @@
+﻿<?php
+    include ('engine/api/api.class.php');
+    $dle_api->install_admin_module ( 'mymod_NotiON', 'Модуль NotiON', 'Модуль списка последних обновлений/редактирований новостей', 'xfset.png', '1' );
+    echo !empty(install_admin_module) ? 'Все хорошо, установилось' : 'Что-то не так...(или просто кривая установка)';
+?>
